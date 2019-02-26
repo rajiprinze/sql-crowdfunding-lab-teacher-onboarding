5ac2f7cd66d287301eaa94a741094d230503dfa5
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-26 16:16:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,19 +7,19 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT projects.title, pledges.amount FROM projects INNER JOIN pledges ON projects.id == pledges.user_id ORDER BY title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, pledges.amount FROM users INNER JOIN pledges ON users.id == pledges.user_id ORDER BY name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, pledges.amount FROM projects INNER JOIN pledges ON projects.id == pledges.user_id WHERE projects.funding_goal <= pledges.amount;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT "
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
